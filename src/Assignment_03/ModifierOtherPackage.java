e7c80c58dd098ef56e917d933c63aa08e47a8e0b
Sabiha Afrin <afrinsabiha516@gmail.com>
2025-05-19 23:41:32 +0600

    Add files via upload

---
+++ src/Assignment_03/ModifierOtherPackage.java
@@ -0,0 +1,20 @@
+package TestModifier;
+
+import Class3.AllModifier;
+
+public class ModifierOtherPackage {
+
+
+    public static void main(String[] args)
+
+    {
+        AllModifier m= new AllModifier();
+        m.UniName="AIUB";
+        m.Location="kuratoli";
+        m.DeptNo=4;
+        m.EstablishedYear=1994;
+        m.PrintData();
+
+    }
+
+}
